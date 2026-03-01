b7f063ef394815f9867f0438804cf2ef13afcb28
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2026-03-01 13:03:25 -0500

    chore: Bump `secrets` formula version to: 0.0.2

--- Formula/secrets.rb
+++ Formula/secrets.rb
@@ -1,8 +1,8 @@
 class Secrets < Formula
   desc "A simple CLI toolset for managing encrypted secrets using GPG"
   homepage "https://www.github.com/jzaleski/secrets"
-  url "https://storage.googleapis.com/jzaleski-files/secrets-0.0.1.tar.gz"
-  sha256 "53d9ea085401074fda44a21c5db4fc7b7d7383b4308e75c6fb79b8ace8ca2a67"
+  url "https://storage.googleapis.com/jzaleski-files/secrets-0.0.2.tar.gz"
+  sha256 "2b62dab0c44a83cea649087ed78beee6b80cf58176d6718f85c4fc34ff05c913"
 
   def install
     bin.install "check-secrets", "decrypt-secrets", "encrypt-secrets"
